cadb66013d71977b68c85337288ffe3073962298
Marian <skrukinickij027@gmail.com>
2026-02-27 22:35:13 +0200

    feat(map): DS-10 add backend for mapservice

---
+++ map-service/go.mod
@@ -0,0 +1,10 @@
+module map-service
+
+go 1.26.0
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.1
+	github.com/golang-migrate/migrate/v4 v4.19.1
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.11.2
+)
---
+++ map-service/internal/models/artifact.go
@@ -0,0 +1,10 @@
+package models
+
+type Artifact struct {
+	ID        int     `json:"id"`
+	Name      string  `json:"name"`
+	Lat       float64 `json:"lat"`
+	Lng       float64 `json:"lng"`
+	Category  string  `json:"category"`
+	CreatedBy int     `json:"created_by"`
+}
